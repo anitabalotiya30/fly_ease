f487355847ecd18c25c22b46023b7b2c20d9d62e
anitabalotiya30 <ideanita2023@gmail.com>
2024-06-06 21:36:58 +0530

    Test

--- lib/modules/sign_up/sign_up_screen.dart
+++ lib/modules/sign_up/sign_up_screen.dart
@@ -15,182 +15,186 @@ class SignupScreen extends StatelessWidget {
       resizeToAvoidBottomInset: true,
 
       //
-      body: Stack(children: [
-        //
-        Image.asset(StrConst.signIn),
-
-        //
-        Align(
-          alignment: Alignment.topCenter,
+      body: Stack(
+        children: [
+          //
+          Image.asset(StrConst.signIn),
 
           //
-          child: Padding(
-            padding: EdgeInsets.only(
-                top: mq.height * .18,
-                right: mq.width * .14,
-                left: mq.width * .04),
+          Align(
+            alignment: Alignment.topCenter,
 
             //
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Padding(
+              padding: EdgeInsets.only(
+                  top: mq.height * .18,
+                  right: mq.width * .14,
+                  left: mq.width * .04),
 
               //
-              children: [
-                //signup
-                const Text(
-                  'Sign up',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 26,
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-
-                // for giving some space
-                SizedBox(height: mq.height * .02),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
 
                 //
-                const Text(
-                  'Connect with DadJoke API and place the joke here to test API connectivity.',
-                  style: TextStyle(
+                children: [
+                  //signup
+                  const Text(
+                    'Sign up',
+                    style: TextStyle(
                       color: Colors.white,
-                      fontSize: 16,
+                      fontSize: 26,
                       fontFamily: 'Roboto',
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  // for giving some space
+                  SizedBox(height: mq.height * .02),
+
+                  //
+                  const Text(
+                    'Connect with DadJoke API and place the joke here to test API connectivity.',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
 
-        /// container inside container
-        Stack(
-          children: [
-            Align(
-              child: Container(
-                margin: EdgeInsets.only(top: mq.height * .38),
-                height: mq.height,
-                width: mq.width,
-
-                //
-                decoration: const BoxDecoration(
-                  //
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(34),
-                      topRight: Radius.circular(34)),
+          /// container inside container
+          Stack(
+            children: [
+              Align(
+                child: Container(
+                  margin: EdgeInsets.only(top: mq.height * .38),
+                  height: mq.height,
+                  width: mq.width,
 
                   //
-                  gradient: SweepGradient(colors: [
-                    Colors.white,
-                    Colors.amber,
-                    Colors.blue,
-                    Colors.purple,
-                    Colors.orange,
-                    Colors.green,
-                  ], center: Alignment.center, startAngle: 3.2, endAngle: 5.4),
-                ),
-              ),
-            ),
-
-            //
-            Align(
-              child: Container(
-                margin: EdgeInsets.only(top: mq.height * .39),
-                padding: EdgeInsets.symmetric(horizontal: mq.width * .04),
-
-                //
-                decoration: const BoxDecoration(
-                    color: Colors.white,
+                  decoration: const BoxDecoration(
+                    //
                     borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(38),
-                        topRight: Radius.circular(38))),
-
-                //
-                child: ListView(
-                  children: [
-                    //
-                    CustomTextField(
-                      etText: TextEditingController(),
-                      label: 'FIRST NAME',
-                    ),
-                    SizedBox(height: mq.height * .02),
+                        topLeft: Radius.circular(34),
+                        topRight: Radius.circular(34)),
 
                     //
-                    CustomTextField(
-                      label: 'LAST NAME',
-                      etText: TextEditingController(),
-                    ),
-                    SizedBox(height: mq.height * .02),
-
-                    //
-                    CustomTextField(
-                      label: 'EMAIL',
-                      etText: TextEditingController(),
-                    ),
-                    SizedBox(height: mq.height * .02),
-
-                    //
-                    CustomTextField(
-                      label: 'PASSWORD',
-                      etText: TextEditingController(),
-                    ),
-                    SizedBox(height: mq.height * .02),
-
-                    //
-                    CustomTextField(
-                      label: 'CONFIRM PASSWORD',
-                      etText: TextEditingController(),
-                    ),
-                  ],
+                    gradient: SweepGradient(
+                        colors: [
+                          Colors.white,
+                          Colors.amber,
+                          Colors.blue,
+                          Colors.purple,
+                          Colors.orange,
+                          Colors.green,
+                        ],
+                        center: Alignment.center,
+                        startAngle: 3.2,
+                        endAngle: 5.4),
+                  ),
                 ),
               ),
-            ),
-
-            //
-            Align(
-              alignment: Alignment.centerRight,
 
               //
-              child: Padding(
-                padding: EdgeInsets.only(bottom: mq.height * .17),
+              Align(
+                child: Container(
+                  margin: EdgeInsets.only(top: mq.height * .39),
+                  padding: EdgeInsets.symmetric(horizontal: mq.width * .04),
+
+                  //
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(38),
+                          topRight: Radius.circular(38))),
+
+                  //
+                  child: ListView(
+                    children: [
+                      //
+                      CustomTextField(
+                        etText: TextEditingController(),
+                        label: 'FIRST NAME',
+                      ),
+                      SizedBox(height: mq.height * .02),
+
+                      //
+                      CustomTextField(
+                        label: 'LAST NAME',
+                        etText: TextEditingController(),
+                      ),
+                      SizedBox(height: mq.height * .02),
+
+                      //
+                      CustomTextField(
+                        label: 'EMAIL',
+                        etText: TextEditingController(),
+                      ),
+                      SizedBox(height: mq.height * .02),
+
+                      //
+                      CustomTextField(
+                        label: 'PASSWORD',
+                        etText: TextEditingController(),
+                      ),
+                      SizedBox(height: mq.height * .02),
+
+                      //
+                      CustomTextField(
+                        label: 'CONFIRM PASSWORD',
+                        etText: TextEditingController(),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              //
+              Align(
+                alignment: Alignment.centerRight,
 
                 //
-                child: InkWell(
-                    onTap: () {
-                      // var a = [1, 2, 3];
-                      // var b = [4, 5, 6];
-                      // a.addAll(b);
-                      // a.sort();
-                      log('numbers ----');
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: mq.height * .17),
 
-                      final numbers = <int>[2, 4, 3];
-                      // Sort from shortest to longest.
-                      numbers.sort((a, b) => a.compareTo(b));
+                  //
+                  child: InkWell(
+                      onTap: () {
+                        // var a = [1, 2, 3];
+                        // var b = [4, 5, 6];
+                        // a.addAll(b);
+                        // a.sort();
+                        log('numbers ----');
 
-                      log('numbers ---- $numbers');
+                        final numbers = <int>[2, 4, 3];
+                        // Sort from shortest to longest.
+                        numbers.sort((a, b) => a.compareTo(b));
 
-                      // int temp = a[0];
-                      // for (int i = 0; i < a.length; i++) {
-                      //   for (int j = i + 1; j < a.length; j++) {
-                      //     if (a[i] > a[j]) {
-                      //       temp = a[i];
-                      //       a[i] = a[j];
-                      //       a[j] = temp;
-                      //     }
-                      //   }
-                      // }
-                    },
-                    //
-                    child:
-                        Image.asset(StrConst.colors, height: mq.height * .16)),
+                        log('numbers ---- $numbers');
+
+                        // int temp = a[0];
+                        // for (int i = 0; i < a.length; i++) {
+                        //   for (int j = i + 1; j < a.length; j++) {
+                        //     if (a[i] > a[j]) {
+                        //       temp = a[i];
+                        //       a[i] = a[j];
+                        //       a[j] = temp;
+                        //     }
+                        //   }
+                        // }
+                      },
+                      //
+                      child: Image.asset(StrConst.colors,
+                          height: mq.height * .16)),
+                ),
               ),
-            ),
-          ],
-        ),
-
-        //
-      ]),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
